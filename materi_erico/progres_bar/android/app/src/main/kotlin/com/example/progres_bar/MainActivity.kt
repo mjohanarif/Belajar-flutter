ff6676b666b46b60464c8fe0a2745b3b76f0ae71
mjohanarif <m.johanarifin@gmail.com>
2022-05-13 22:14:48 +0700

    delete unused

--- materi_erico/progres_bar/android/app/src/main/kotlin/com/example/progres_bar/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.progres_bar
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- materi_erico/progres_bars/android/app/src/main/kotlin/com/example/progres_bars/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.progres_bars
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
